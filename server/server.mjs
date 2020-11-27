f3ccc0b8b0929aecff672b5862c7995f8e774b1d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-27 14:56:02 +0100

    create user schema

---
+++ server/server.mjs
@@ -0,0 +1,24 @@
+import config from './../config/config.mjs';
+import mongoose from 'mongoose';
+
+import app from './express.mjs';
+
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
+})
+
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
+})
