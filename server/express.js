381a417a6c3dac2f2f2d1c82884421dff966f443
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-27 11:23:46 +0100

    project set up

---
+++ server/express.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from cors
+import helmet from 'helmet'
+
+
+
+
+const app = express()
+
+
+// configure express
+export default app;
